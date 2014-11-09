7c8c42645c7a38bd48f775eebc5a68be3c5c00bf
Anjo Vahldiek <vahldiek@mpi-sws.org>
2014-11-09 16:33:25 +0100

    updated connection

--- rpc/connection.cc
+++ rpc/connection.cc
@@ -349,13 +349,15 @@ tcpsconn::process_accept()
 
         // garbage collect all dead connections with refcount of 1
         std::map<int, connection *>::iterator i;
-        for (i = conns_.begin(); i != conns_.end(); i++) {
+        for (i = conns_.begin(); i != conns_.end(); ) {
                 if (i->second->isdead() && i->second->ref() == 1) {
 			jsl_log(JSL_DBG_2, "accept_loop garbage collected fd=%d\n",
 					i->second->channo());
                         i->second->decref();
-                        conns_.erase(i);
-                }
+                        conns_.erase(i++);
+                } 
+		else
+		  i++;
         }
 
 	conns_[ch->channo()] = ch;
